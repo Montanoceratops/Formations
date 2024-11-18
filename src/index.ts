603226c3e1ca982edb837e5785cb24adbe4322fe
Montanoceratops <136097691+Montanoceratops@users.noreply.github.com>
2024-11-18 16:55:01 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,9 @@ const options: MapOptions = {
 }
 const formationsMap = map('map', options).setView([0, 0], 2);
 
-tileLayer('https://tile.openstreetmap.org/{zoom}/{x}/{y}.png', {
+tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
-    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+    attribution: '<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 
 }).addTo(formationsMap);
 
