e384a261daebd31a85f867eeee47c80e72f599fb
solonovamax <solonovamax@12oclockpoint.com>
2025-02-03 02:22:03 -0500

    Fix a few issues, apply changes from upstream

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,41 @@
-import {map, MapOptions, tileLayer} from 'leaflet';
+import {map, MapOptions, markerClusterGroup, MarkerOptions, Popup, tileLayer} from 'leaflet';
 import 'leaflet.markercluster';
 import * as works from "./works/*/marker.ts";
 
-const options: MapOptions = {
-
-}
+const options: MapOptions = {}
 const formationsMap = map('map', options).setView([0, 0], 2);
 
+// maybe consider https://openfreemap.org/?
 tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-
     attribution: '<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-
 }).addTo(formationsMap);
 
 // add all markers
+let markerGroup = markerClusterGroup()
+
+let markerOptions: MarkerOptions = {
+    autoPan: true,
+    autoPanPadding: [16, 16]
+}
 
 for (let work of Object.values(works)) {
-    work["default"](formationsMap)
+    markerGroup.addLayer(
+        // magic
+        work["default"](markerOptions)
+    )
 }
+
+formationsMap.addLayer(markerGroup)
+
+// misplaced popup workaround
+document.querySelector(".leaflet-popup-pane").addEventListener("load", function (event) {
+    // @ts-ignore
+    let tagName: string = event.target.tagName;
+    // @ts-ignore
+    let popup: Popup = formationsMap._popup; // Currently open popup, if any.
+
+    if (tagName === "IMG" || tagName === "PICTURE" && popup && !popup["_updated"]) {
+        popup["_updated"] = true; // prevent infinite loop
+        popup.update();
+    }
+}, true); // Capture the load event, because it does not bubble.
--- src/works/jebel/marker.ts
+++ src/works/jebel/marker.ts
@@ -1,11 +1,10 @@
-import {Map, marker, Marker} from 'leaflet';
+import {marker, Marker, MarkerOptions} from 'leaflet';
 import {createPopup} from '/src/markers';
 import metadata from './metadata.json';
 import popup from 'bundle-text:./popup.html';
 
 // noinspection JSUnusedGlobalSymbols
-export default function createMarker(map: Map): Marker {
-    return marker(metadata.coordinates, {title: metadata.tooltip})
+export default function createMarker(options: MarkerOptions): Marker {
+    return marker(metadata.coordinates, {...options, title: metadata.tooltip})
         .bindPopup(createPopup(popup, metadata))
-        .addTo(map)
 }
