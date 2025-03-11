0a66a9f98240bf535b4110c8a63220175af80ba7
Montanoceratops <136097691+Montanoceratops@users.noreply.github.com>
2025-03-10 19:55:25 -0600

    Update marker.ts

--- src/works/Ischigualasto/marker.ts
+++ src/works/Ischigualasto/marker.ts
@@ -1,10 +1,10 @@
-import {marker, Marker, MarkerOptions} from 'leaflet';
-import {createPopup} from '/src/markers';
-import metadata from './metadata.json';
-import popup from 'bundle-text:./popup.html';
+import {Marker} from "leaflet";
+import {createMarker} from "/src/markers";
+import metadata from "./metadata.json";
+import popup from "bundle-text:./popup.html";
 
 // noinspection JSUnusedGlobalSymbols
-export default function createMarker(options: MarkerOptions): Marker {
-    return marker(metadata.coordinates, {...options, title: metadata.tooltip})
-        .bindPopup(createPopup(popup, metadata))
+export function marker(): Marker[] {
+    return createMarker(popup, metadata);
 }
+
